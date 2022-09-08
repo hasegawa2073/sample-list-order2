7f2f08d49f4db8b3d5d128e0ac65262136e73065
hasegawa2073 <hasetatsu2001@icloud.com>
2022-09-08 18:06:55 +0900

    add: 掴んだリストをマウスの位置に合わせて移動させる

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,19 +14,51 @@ document.addEventListener('DOMContentLoaded', function () {
         listTopArray.push(accu);
         return accu + curr;
     }, 0);
-    // リストそれぞれにtopの値を割り当てる
+    // リストそれぞれに絶対配置の値を割り当てる
     lists.forEach((list, index) => {
         const target = list;
         target.style.top = `${listTopArray[index]}px`;
+        target.style.left = '0px';
     });
+    // マウスが入ってきた位置の変数
+    let startMouseX;
+    let startMouseY;
+    // マウスを動かしているときのマウスの位置の変数
+    let currentMouseX;
+    let currentMouseY;
+    // マウスの移動距離の変数
+    let moveMouseX;
+    let moveMouseY;
+    // 移動前のリストの座標
+    let startTargetX;
+    let startTargetY;
     lists.forEach((list) => {
         const target = list;
         target.addEventListener('mousedown', function (e) {
             const currentTarget = e.currentTarget;
+            startMouseX = e.clientX;
+            startMouseY = e.clientY;
+            startTargetX = parseInt(currentTarget.style.left);
+            startTargetY = parseInt(currentTarget.style.top);
             currentTarget.classList.add('grabbing');
+        });
+        target.addEventListener('mousemove', function (e) {
+            const currentTarget = e.currentTarget;
+            currentMouseX = e.clientX;
+            currentMouseY = e.clientY;
+            moveMouseX = currentMouseX - startMouseX;
+            moveMouseY = currentMouseY - startMouseY;
+            // リストを掴んだままマウスを移動しているとき
+            if (currentTarget.classList.contains('grabbing')) {
+                const moveTargetX = moveMouseX;
+                const moveTargetY = startTargetY + moveMouseY;
+                target.style.left = `${moveTargetX}px`;
+                target.style.top = `${moveTargetY}px`;
+            }
         });
         target.addEventListener('mouseup', function (e) {
             const currentTarget = e.currentTarget;
+            currentTarget.style.left = '0px';
             currentTarget.classList.remove('grabbing');
         });
         target.addEventListener('mouseout', function (e) {
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -13,19 +13,53 @@ document.addEventListener('DOMContentLoaded', function () {
     listTopArray.push(accu);
     return accu + curr;
   }, 0);
-  // リストそれぞれにtopの値を割り当てる
+  // リストそれぞれに絶対配置の値を割り当てる
   lists.forEach((list, index) => {
     const target: HTMLElement = list as HTMLElement;
     target.style.top = `${listTopArray[index]}px`;
+    target.style.left = '0px';
   });
+
+  // マウスが入ってきた位置の変数
+  let startMouseX: number;
+  let startMouseY: number;
+  // マウスを動かしているときのマウスの位置の変数
+  let currentMouseX: number;
+  let currentMouseY: number;
+  // マウスの移動距離の変数
+  let moveMouseX: number;
+  let moveMouseY: number;
+  // 移動前のリストの座標
+  let startTargetX: number;
+  let startTargetY: number;
+
   lists.forEach((list) => {
     const target: HTMLElement = list as HTMLElement;
     target.addEventListener('mousedown', function (e: any) {
       const currentTarget: HTMLElement = e.currentTarget;
+      startMouseX = e.clientX;
+      startMouseY = e.clientY;
+      startTargetX = parseInt(currentTarget.style.left);
+      startTargetY = parseInt(currentTarget.style.top);
       currentTarget.classList.add('grabbing');
+    });
+    target.addEventListener('mousemove', function (e: any) {
+      const currentTarget: HTMLElement = e.currentTarget;
+      currentMouseX = e.clientX;
+      currentMouseY = e.clientY;
+      moveMouseX = currentMouseX - startMouseX;
+      moveMouseY = currentMouseY - startMouseY;
+      // リストを掴んだままマウスを移動しているとき
+      if (currentTarget.classList.contains('grabbing')) {
+        const moveTargetX = moveMouseX;
+        const moveTargetY = startTargetY + moveMouseY;
+        target.style.left = `${moveTargetX}px`;
+        target.style.top = `${moveTargetY}px`;
+      }
     });
     target.addEventListener('mouseup', function (e: any) {
       const currentTarget: HTMLElement = e.currentTarget;
+      currentTarget.style.left = '0px';
       currentTarget.classList.remove('grabbing');
     });
     target.addEventListener('mouseout', function (e: any) {
